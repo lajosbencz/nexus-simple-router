173206479bcf4966e215801784d1a055c765d763
lazos <lazos@lazos.me>
2022-08-31 02:18:19 +0200

    websocket/rawsocket transport

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ go install github.com/lajosbencz/nexus-simple-router@latest
 ## Flags
 
  - -realm _string_
- - -host _string_
- - -port _int_
+ - -ws _bool_
+ - -ws-host _string_
+ - -ws-port _int_
+ - -rs _bool_
+ - -rs-host _string_
+ - -rs-port _int_
  - -decho _bool_
+ 
--- main.go
+++ main.go
@@ -16,8 +16,10 @@ import (
 
 var (
 	realm       = "default"
-	wsHost      = "localhost"
+	wsHost      = "0.0.0.0"
 	wsPort      = 8951
+	rsHost      = "127.0.0.1"
+	rsPort      = 8952
 	localClient *client.Client
 	logger      *log.Logger
 	devEcho     = true
@@ -26,8 +28,12 @@ var (
 func main() {
 
 	flag.StringVar(&realm, "realm", "default", "Realm to be created")
-	flag.StringVar(&wsHost, "host", "localhost", "Host to listen on")
-	flag.IntVar(&wsPort, "port", 8951, "Port to listen on")
+	flag.BoolVar(&devEcho, "ws", true, "Should WebSocket transport be started")
+	flag.StringVar(&wsHost, "ws-host", "localhost", "WebSocket host to listen on")
+	flag.IntVar(&wsPort, "ws-port", 8951, "WebSocket port to listen on")
+	flag.BoolVar(&devEcho, "rs", true, "Should RawSocket transport be started")
+	flag.StringVar(&rsHost, "rs-host", "localhost", "RawSocket host to listen on")
+	flag.IntVar(&rsPort, "rs-port", 8952, "RawSocket port to listen on")
 	flag.BoolVar(&devEcho, "decho", true, "Should dev.echo RPC be registered")
 	flag.Parse()
 	wsAddr := fmt.Sprintf("%s:%d", wsHost, wsPort)
@@ -64,12 +70,14 @@ func main() {
 	wsServer.Upgrader.EnableCompression = true
 	wsServer.EnableTrackingCookie = true
 	wsServer.KeepAlive = 30 * time.Second
-
 	wsCloser, err := wsServer.ListenAndServe(wsAddr)
 	if err != nil {
 		panic(err)
 	}
 	defer wsCloser.Close()
+
+	rsServer := router.NewRawSocketServer(wsRouter)
+	rsServer.KeepAlive = 30 * time.Second
 
 	if devEcho {
 		err = createLocalCallee(localClient, "dev.echo", func(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
